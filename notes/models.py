606ab58ab25269cd7fc3769e3d34a5e9084f0243
Kamil Fryszkowski <kamil.fryszkowski@idealgroup.com.pl>
2024-01-20 14:12:44 +0100

    Setup project and initial migration with encrypted Notes

---
+++ notes/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from django.contrib.auth.models import User
+import hashlib
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes', null=True, blank=True, default=None)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.TextField()
+    public = models.BooleanField(default=False, blank=True)
+    encrypted = models.BooleanField(default=False, blank=True)
+    password = models.TextField(blank=True, default="")
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.public:
+            self.encrypted = False
+        
+        if self.encrypted:
+            text = self.content
+            password = self.password
+            
+            salt_length = len(str(self.title)) % 5
+            salt = f"g7{str(self.title).ljust(salt_length)}pj"
+            salted_password = salt + password
+            hasher = hashlib.sha256()
+            password_bytes = salted_password.encode('utf-8')
+            hasher.update(password_bytes)
+            hashed_password = hasher.hexdigest()
+            
+            key = hashed_password.ljust(32)[:32].encode()
+            cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+            encryptor = cipher.encryptor()
+            padder = padding.PKCS7(128).padder()
+            padded_data = padder.update(text.encode('utf-8')) + padder.finalize()
+            encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+            encrypted_text = encrypted_data.decode('latin-1')
+            
+            self.password = hashed_password
+            self.content = encrypted_text
+            
+        super().save(*args, **kwargs)
+
+    def get_decrypted_text(self, password):
+        if not self.encrypted:
+            return self.content
+        
+        salt_length = len(str(self.title)) % 5
+        salt = f"g7{str(self.title).ljust(salt_length)}pj"
+        salted_password = salt + password
+        hasher = hashlib.sha256()
+        password_bytes = salted_password.encode('utf-8')
+        hasher.update(password_bytes)
+        hashed_password = hasher.hexdigest()
+        
+        if hashed_password != self.password:
+            raise Exception('Wrong password')
+        
+        key = hashed_password.ljust(32)[:32].encode()
+        cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+        decryptor = cipher.decryptor()
+        encrypted_data = self.content.encode('latin-1')
+        decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        upadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+        
+        decrypted_text = upadded_data.decode('utf-8')
+        
+        return decrypted_text
+    
+    def get_encrypted_text(self):
+        return self.content
